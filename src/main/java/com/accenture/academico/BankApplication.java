03aafb46c7f4372401abaa91abf2a3b6137d77d4
breno duarte <brenoo.duarte@hotmail.com>
2024-08-01 13:24:06 -0300

    fix: change jdk version and add dependencies

--- src/main/java/com/accenture/academico/BankApplication.java
+++ src/main/java/com/accenture/academico/BankApplication.java
@@ -1,4 +1,4 @@
-package com.academy.bank;
+package com.accenture.academico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/accenture/academico/BankApplicationTests.java
+++ src/test/java/com/accenture/academico/BankApplicationTests.java
@@ -1,4 +1,4 @@
-package com.academy.bank;
+package com.accenture.academico;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
